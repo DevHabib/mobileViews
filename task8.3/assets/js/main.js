397cde5133927de69434bbc8122271507eac908d
Md Habibur Rahman <contact@devhabib.com>
2020-03-20 01:19:43 +0600

    task 8.3 is completed perfectly

--- task8.3/assets/js/main.js
+++ task8.3/assets/js/main.js
@@ -11,6 +11,15 @@ var swiper = new Swiper('.swiper-container', {
     shadowOffset: 20,
     shadowScale: 0.04,
   },
- 
+
   pagination: '.swiper-pagination'
+});
+
+$(document).ready(function () {
+  $("#customRadioInline1").click(function () {
+    $(".custom-1").toggleClass("color");
+  });
+  $("#customRadioInline2").click(function () {
+    $(".custom-2").toggleClass("color");
+  });
 });
