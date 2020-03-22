93b0e78699580ea708e7b2dc4d1110df8e2db83c
Md Habibur Rahman <habib@yotech.ltd>
2020-03-22 16:13:24 +0600

    Task 8.4 Completed

---
+++ task8.4/assets/js/main.js
@@ -0,0 +1,25 @@
+var swiper = new Swiper('.swiper-container', {
+  effect: 'flip',
+  grabCursor: true,
+  cssMode: true,
+  loop: true,
+  clickable: true,
+  clickableClass: 'swiper-pagination-clickable',
+  cubeEffect: {
+    shadow: true,
+    slideShadows: true,
+    shadowOffset: 20,
+    shadowScale: 0.04,
+  },
+
+  pagination: '.swiper-pagination'
+});
+
+$(document).ready(function () {
+  $("#customRadioInline1").click(function () {
+    $(".custom-1").toggleClass("color");
+  });
+  $("#customRadioInline2").click(function () {
+    $(".custom-2").toggleClass("color");
+  });
+});
