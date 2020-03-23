35eac61144b66ae46fc134352e565944edb0e58c
Md Habibur Rahman <contact@devhabib.com>
2020-03-24 00:41:43 +0600

    This mobile view is developing with materialize. (Incomplete Work)

--- task8.5/assets/js/main.js
+++ task8.5/assets/js/main.js
@@ -1,14 +1,5 @@
-var swiper = new Swiper('.swiper-container', {
-  effect: 'flip',
-  grabCursor: true,
-  cssMode: true,
-  loop: true,
-  clickable: true,
-  clickableClass: 'swiper-pagination-clickable',
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  autoplay: 2000,
-  pagination: '.swiper-pagination'
+$(document).ready(function () {
+  $('.sidenav').sidenav();
+  $('.tabs').tabs();
+  $('.collapsible').collapsible();
 });
